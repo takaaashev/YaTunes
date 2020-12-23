3fd550abf16fa35e8406301ac5a043885b5ac33d
takaaashev <kanekki28@gmail.com>
2020-12-23 20:07:17 +0500

    radioPLayer

--- scripts/musicPlayer.js
+++ scripts/musicPlayer.js
@@ -1,3 +1,59 @@
 export const musicPlayerInit = () => {
-    console.log('music Init');
+    const radio = document.querySelector('.radio');
+    const radioCoverImg = document.querySelector('.radio-cover__img');
+    const radioNavigation = document.querySelector('.radio-navigation');
+    const radioHeaderBig = document.querySelector('.radio-header__big');
+    const radioItem = document.querySelectorAll('.radio-item');
+    const radioStop = document.querySelector('.radio-stop');
+
+    const audio = new Audio();
+    audio.type = 'audio/aac';
+
+    radioStop.diasbled = true;
+
+    const changeIconPlay = () => {
+        if (audio.paused) {
+            radio.classList.remove('play');
+            radioStop.classList.add('fa-play');
+            radioStop.classList.remove('fa-stop');
+        } else {
+            radio.classList.add('play');
+            radioStop.classList.add('fa-stop');
+            radioStop.classList.remove('fa-play');
+        }
+    }
+
+    const selectItem = elem => {
+        radioItem.forEach(item => item.classList.remove('select'));
+        elem.classList.add('select');
+    }
+
+    radioNavigation.addEventListener('change', event => {
+        const target = event.target;
+        const parrent = target.closest('.radio-item');
+        selectItem(parrent);
+
+        const title = parrent.querySelector('.radio-name').textContent;
+        radioHeaderBig.textContent = title;
+
+        const UrlImg = parrent.querySelector('.radio-img').src;
+        radioCoverImg.src = UrlImg;
+
+
+        radioStop.diasbled = false;
+        audio.src = target.dataset.radioStantion;
+
+        audio.play();
+        changeIconPlay();
+
+    });
+
+    radioStop.addEventListener('click', () => {
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+        changeIconPlay();
+    })
 };
